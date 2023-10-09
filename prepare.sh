1d813c5ee57ce78fe748ea26622fc6dac2fef5df
Alexandru Dura <alexandru.dura@cs.lth.se>
2023-10-09 10:53:57 +0200

    A script to prepare the Magma repos for our needs

---
+++ prepare.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+PROJECTS=(libpng)
+
+prepare_project() {
+    proj=$1
+
+    proj_dir=`pwd`/targets/"$proj"
+
+    export TARGET=$proj_dir
+    export OUT=$TARGET/out
+    export CC="gcc"
+
+    mkdir -p $OUT
+
+    # fetch
+    pushd $proj_dir
+    ./fetch.sh
+    popd
+
+    # patch - introduce the bugs
+    ./magma/apply_patches.sh
+
+    # build - sanity check
+    pushd $proj_dir
+    bear ./build.sh
+    popd
+}
+
+for p in $PROJECTS ; do
+    echo "Preparing project $p"
+    prepare_project "$p"
+done
